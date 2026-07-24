96ac5bd613cf61b90a2c57bbe9479a31ac6bb06c
Rahul Rajak <09rcrg@gmail.com>
2026-07-24 15:38:03 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,54 +3,6 @@ export default {
 
     const url = new URL(request.url);
 
-    if (url.pathname === "/") {
-
-      return new Response(
-        JSON.stringify({
-          status: "online",
-          message: "Rahul Social Hub API Running 🚀"
-        }),
-        {
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-      );
-
-    }
-
-
-    if (url.pathname === "/api/test") {
-
-      const result = await env.DB
-        .prepare("SELECT name FROM sqlite_master WHERE type='table'")
-        .all();
-
-      return new Response(
-        JSON.stringify({
-          message: "D1 Database Connected ✅",
-          tables: result.results
-        }),
-        {
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-      );
-
-    }
-
-
-    return new Response(
-      "Rahul Social Hub Server"
-    );
-
-  }
-};export default {
-  async fetch(request, env) {
-
-    const url = new URL(request.url);
-
     // Home
     if (url.pathname === "/") {
       return new Response(
