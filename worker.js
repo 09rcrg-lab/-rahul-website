1507478a3c454333c16484caff77a58d6fb1d000
Rahul Rajak <09rcrg@gmail.com>
2026-07-28 19:24:18 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,8 +3,38 @@ export default {
 
     const url = new URL(request.url);
 
+    // Home API
     if (url.pathname === "/") {
       return new Response("Rahul Social Hub API Running 🚀");
+    }
+
+    // Register API
+    if (url.pathname === "/api/register" && request.method === "POST") {
+
+      const { username, email, password } = await request.json();
+
+      try {
+
+        await env.DB.prepare(
+          "INSERT INTO users (username,email,password,coins) VALUES (?,?,?,?)"
+        )
+        .bind(username, email, password, 0)
+        .run();
+
+        return Response.json({
+          success: true,
+          message: "User Registered Successfully"
+        });
+
+      } catch (e) {
+
+        return Response.json({
+          success: false,
+          message: "Email already registered"
+        });
+
+      }
+
     }
 
     return new Response("API Not Found", {
